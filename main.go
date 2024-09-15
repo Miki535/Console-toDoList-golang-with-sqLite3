12a07d0636a7e2b28381436872b410874a9c51fc
Taras Shcherban <147646967+Miki535@users.noreply.github.com>
2024-09-15 11:07:26 +0000

    Update

--- main.go
+++ main.go
@@ -12,15 +12,16 @@ import (
 )
 
 var name, information string
+var id int
 var db *sql.DB
 
 func main() {
-	var choose int8
+	var choose1, choose2 int8
 	fmt.Println("Hello!")
 	for {
-		fmt.Println("You can choose:\n1)New\n2)Get All\n3)Delete\n4)Get one\n5)Exit")
-		fmt.Scan(&choose)
-		switch choose {
+		fmt.Println("You can choose:\n1)New\n2)Get All\n3)Delete\n4)Get one\n5)Delete All\n6)Exit")
+		fmt.Scan(&choose1)
+		switch choose1 {
 		case 1:
 			fmt.Println("Enter name for youre ToDO:")
 			fmt.Scan(&name)
@@ -38,6 +39,41 @@ func main() {
 		case 2:
 			fmt.Println("Youre All Info:")
 			GetAll()
+		case 3:
+			fmt.Println("Choose:\n1)Delete by name\n2)Delete by id\n3)Exit")
+			fmt.Scan(&choose2)
+			switch choose2 {
+			case 1:
+				fmt.Println("Enter name for today:")
+				fmt.Scan(&name)
+				deleteByName(name)
+			case 2:
+				fmt.Println("Enter id for today:")
+				fmt.Scan(&id)
+				deleteById(id)
+			case 3:
+				fmt.Println("Youre Welcome!")
+			}
+		case 4:
+			fmt.Println("Choose:\n1)Get by name\n2)Get by id\n3)Exit")
+			fmt.Scan(&choose2)
+			switch choose2 {
+			case 1:
+				fmt.Println("Enter name for today:")
+				fmt.Scan(&name)
+				getByName(name)
+			case 2:
+				fmt.Println("Enter id for today:")
+				fmt.Scan(&id)
+				getById(id)
+			case 3:
+			}
+
+		case 5:
+			deletAll()
+		case 6:
+			fmt.Println("Bye!")
+			os.Exit(0)
 		}
 	}
 }
@@ -86,5 +122,63 @@ func GetAll() {
 			log.Fatal(err)
 		}
 		fmt.Println("Id: ", id, "Name:", name, "Information:", information)
+	}
+}
+
+func deletAll() {
+	query := `DELETE FROM infos`
+	_, err := db.Exec(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func deleteByName(name string) {
+	query := `DELETE FROM infos WHERE name = ?`
+	_, err := db.Exec(query, name)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("Deleted info:", name)
+	}
+}
+
+func deleteById(id int) {
+	query := `DELETE FROM infos WHERE id = ?`
+	_, err := db.Exec(query, id)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("Deleted info by id:", id)
+	}
+}
+
+func getByName(name string) {
+	query := `SELECT information FROM infos WHERE name = ?`
+	rows, err := db.Query(query, name)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		err = rows.Scan(&name, &information)
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			fmt.Println(information)
+		}
+	}
+}
+
+func getById(id int) {
+	query := `SELECT information FROM infos WHERE id = ?`
+	rows, err := db.Query(query, id)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		err = rows.Scan(&id, &information)
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			fmt.Println(information)
+		}
 	}
 }
